cb31dd4ced1220c7466452b535b4ef84d8189309
Denisse <denisse.lh96@gmail.com>
2026-05-08 13:54:07 -0400

    Agregar Ejercicios.js

---
+++ ejercicios.js
@@ -0,0 +1,94 @@
+let nombre = "Denisse";
+let apellido = "Labraña";
+
+console.log("Hola soy " + nombre + " " + apellido);
+
+// Variable numero
+let numero = 10;
+numero = numero + 5;
+
+console.log(numero);
+
+numero = numero * 3;
+
+console.log(numero);
+
+// Comida favorita
+let comidaFavorita = "Sushi";
+
+console.log(comidaFavorita);
+
+// Variables
+let a = 10;
+let b = 3;
+
+console.log(a);
+console.log(b);
+
+console.log("Suma:", a + b);
+console.log("Resta:", a - b);
+console.log("Multiplicación:", a * b);
+console.log("Módulo:", a % b);
+
+// Ambos como string
+let c = "10";
+let d = "3";
+
+console.log("Suma strings:", c + d);
+console.log("Resta strings:", c - d);
+console.log("Multiplicación strings:", c * d);
+console.log("Módulo strings:", c % d);
+
+// String y number
+let e = "10";
+let f = 3;
+
+console.log("Suma string + number:", e + f);
+console.log("Resta string - number:", e - f);
+console.log("Multiplicación string * number:", e * f);
+console.log("Módulo string % number:", e % f);
+
+// Crear una variable 
+let edad = 15;
+
+console.log("Mayor a 10:", edad > 10);
+console.log("Menor a 18:", edad < 18);
+
+// Crea una variable numero = 8
+numero = 8;
+
+console.log(numero > 5);
+console.log(numero < 10);
+console.log(numero > 5 && numero < 10);
+
+// Crea una variable nota = 6
+let nota = 6;
+
+console.log("Es igual a 7:", nota === 7);
+
+// Crea una variable temperatura = 25
+let temperatura = 25
+
+console.log("Es mayor o igual a 30:", temperatura >= 30);
+
+// Crear dos numeros
+let num1 = 10;
+let num2 = 20;
+
+console.log("Son iguales:", num1 === num2);
+console.log("num1 es mayor que num2:", num1 > num2);
+console.log("num2 es mayor que num1:", num2 > num1);
+
+// Crear 1
+edad = 17;
+let tienePermiso = true;
+
+console.log("Puedo conducir:",edad >= 18 || tienePermiso);
+
+// Crear 2
+let usuario = "admin";
+let password = "1234";
+
+console.log("Acceso correcto:",usuario === "admin" && password === "1234");
+
+
